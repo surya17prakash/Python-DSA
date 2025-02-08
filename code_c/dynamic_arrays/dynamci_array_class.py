26982a441f32a0c4f09123bf02a7cddadba7d79e
surya prakash <95066571+surya17prakash@users.noreply.github.com>
2025-02-08 13:54:30 +0530

    sample

--- code_c/dynamic_arrays/dynamci_array_class.py
+++ code_c/dynamic_arrays/dynamci_array_class.py
@@ -1,1 +1,21 @@
 print("hello world")
+import ctypes
+
+class DynamicArray(object):
+    def __init__(self):
+        self.n=0
+        self.capacity=1
+        self.A=self.make_array(self.capacity)
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self,k):
+        if not 0 <= k < self.n:
+            return IndexError('k is out of bound') 
+        return self.A[k]
+    
+    def append(self,ele):
+        if self.n == self.capacity:
+            self._resize(2*self.capacity)
+
